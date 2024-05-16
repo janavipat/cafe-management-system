d98e06f81d3b0b79865f0312a5d53e1925a05c06
lanet <lanet@gmail.com>
2024-05-16 20:44:06 +0530

    add category api

---
+++ models/category.js
@@ -0,0 +1,27 @@
+// category.js
+
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+
+const categorySchema = new mongoose.Schema(
+  {
+   id:{
+    type: String,
+    default: uuidv4,
+    required: true,
+    unique: true,
+   },
+   name:{
+    type: String,
+    required: true,
+    
+   },
+  },
+  {
+    collection: "category",
+  }
+);
+
+const Category = mongoose.model("Category", categorySchema); // Change model name to "Category"
+
+module.exports = Category;
