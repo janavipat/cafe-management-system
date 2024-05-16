2ec854d5f4bf423b3f0f71f533c691a08a56eaeb
lanet <lanet@gmail.com>
2024-05-16 22:44:32 +0530

    add category delete api

--- routes/category.js
+++ routes/category.js
@@ -60,5 +60,23 @@ router.put(
   }
 );
 
+router.delete('/delete/:categoryId', auth.authenticateToken, async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
 
+    // Check if the category exists
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    // Delete the category
+    await Category.findByIdAndDelete(categoryId);
+
+    res.status(200).json({ message: 'Category deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting category:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 module.exports = router;
